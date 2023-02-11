d1efafa8a09d6189a122c0d692751144b05507da
Игорь Любаев <Lyubaev85@gmail.com>
2023-02-11 15:21:18 +0300

    fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,9 +3,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
-from posts.models import Comment, Follow, Group, Post
-
-User = get_user_model()
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -49,7 +47,7 @@ class FollowSerializer(serializers.ModelSerializer):
                 queryset=Follow.objects.all(), fields=('following', 'user'))
         ]
 
-    def validate(self, data):
-        if self.context['request'].user == data['following']:
+    def validate_following(self, value):
+        if self.context['request'].user == value:
             raise serializers.ValidationError('Нельзя подписаться на себя.')
-        return data
+        return value
